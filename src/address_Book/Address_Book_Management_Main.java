918004582ef0945e0ad45d6eb722490fa3695544
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-03 01:47:35 +0530

    changes uc1

--- src/address_Book/Address_Book_Management_Main.java
+++ src/address_Book/Address_Book_Management_Main.java
@@ -3,5 +3,24 @@ package address_Book;
 public class Address_Book_Management_Main {
     public static void main(String[] args) {
 
+
     }
+}
+
+class Contact{
+    //String name,String lasttN,String city,String number,String email
+    private String name;
+    private String last_N;
+    private String city;
+    private String number;
+    private String email;
+
+    public Contact(String name,String lasttN,String city,String number,String email){
+        this.name = name;
+        this.last_N = lasttN;
+        this.city = city;
+        this.number = number;
+        this.email = email;
+    }
+
 }
