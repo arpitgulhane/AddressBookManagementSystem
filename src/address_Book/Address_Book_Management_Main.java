7d7eb99951c5cfcd626941eacf09b014fe733745
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-10 13:12:40 +0530

    cut spaces

--- src/address_Book/Address_Book_Management_Main.java
+++ src/address_Book/Address_Book_Management_Main.java
@@ -26,43 +26,43 @@ public class Address_Book_Management_Main {
                if (getBookName.equals(addBook.getBookName())) {
                    System.out.println(addBook+" AddressBook already present ");
                    check = 0;
-               } else {
+               }
+               else {
                    AddressBook addressBook = new AddressBook(getBookName);
                    addressBook_arr.add(addressBook);
                    System.out.println("Address Book Added ...");
                    check = 0;
-//               System.out.println(" if You Want to add Contact   Enter Number 1(add) , 0(Exit) , 2(edit)  ,3(delete)  ");
-               Scanner sc =new Scanner(System.in);
+                   Scanner sc =new Scanner(System.in);
                    System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  :: To EXIT ( 0 ZERO ) ~ ~ ~             ");
-        int exit=sc.nextInt();
-        while (exit!=0){
-                   addressBook.addPersonDetails();
-                   addressBook.editPersonDetails();
-                   addressBook.deletePersonDetails();
-        System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To Exit press (0 ZERO) ~ ~ ~             ");
-        exit =sc.nextInt();
-    }
+                    int exit=sc.nextInt();
+                    while (exit!=0){
+                        addressBook.addPersonDetails();
+                        addressBook.editPersonDetails();
+                        addressBook.deletePersonDetails();
+                        System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To Exit press (0 ZERO) ~ ~ ~             ");
+                        exit =sc.nextInt();
+                    }
 // --------------------------------------------------------
                }
            }
-           if (check == 1) {
-               AddressBook addressBook = new AddressBook(getBookName);
-               addressBook_arr.add(addressBook);
-               System.out.println("Address Book Added ...");
-               //               System.out.println(" if You Want to add Contact   Enter Number 1(add) , 0(Exit) , 2(edit)  ,3(delete)  ");
-               Scanner sc =new Scanner(System.in);
-               System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To Exit press (0 ZERO) ~ ~ ~             ");
-        int exit=sc.nextInt();
-        while (exit!=0){
-               addressBook.addPersonDetails();
-               addressBook.editPersonDetails();
-               addressBook.deletePersonDetails();
-            System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To EXIT press (0 ZERO) ~ ~ ~             ");
-        exit =sc.nextInt();
-    }
-           }
-           System.out.println("             ~ ~ ~ press 1 to 9 add onemore ADDRESS BOOK  ::  Exit ( 0 ZERO ) ~ ~ ~              ");
-           getMoreBook=get.nextInt();
+                   if (check == 1) {
+                       AddressBook addressBook = new AddressBook(getBookName);
+                       addressBook_arr.add(addressBook);
+                       System.out.println("Address Book Added ...");
+                       //               System.out.println(" if You Want to add Contact   Enter Number 1(add) , 0(Exit) , 2(edit)  ,3(delete)  ");
+                       Scanner sc =new Scanner(System.in);
+                       System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To Exit press (0 ZERO) ~ ~ ~             ");
+                        int exit=sc.nextInt();
+                            while (exit!=0){
+                                   addressBook.addPersonDetails();
+                                   addressBook.editPersonDetails();
+                                   addressBook.deletePersonDetails();
+                                   System.out.println("             ~ ~ ~ ADD NEW CONTACT press 1-9  ::  To EXIT press (0 ZERO) ~ ~ ~             ");
+                                   exit =sc.nextInt();
+                            }
+                   }
+               System.out.println("             ~ ~ ~ press 1 to 9 add onemore ADDRESS BOOK  ::  Exit ( 0 ZERO ) ~ ~ ~              ");
+               getMoreBook=get.nextInt();
        }while (getMoreBook==1);
    }
 }
